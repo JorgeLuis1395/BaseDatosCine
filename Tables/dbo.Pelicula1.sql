b5214bb855d7829b0c65844a0404ded9b2bb68ee
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-05-19 19:57:21 -0500

    Cambio

---
+++ Tables/dbo.Pelicula1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Pelicula1]
+(
+[id_Pelicula] [int] NULL,
+[Nombre_Pelicula] [nchar] (10) COLLATE Modern_Spanish_CI_AS NULL,
+[aActores] [nchar] (10) COLLATE Modern_Spanish_CI_AS NULL
+) ON [PRIMARY]
+GO
